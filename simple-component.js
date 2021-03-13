66857d694faa59bbb6c88950afafa6738dc2e18a
Brendan Keogh <beeker@brendankeogh.com>
2021-03-12 21:13:18 -0700

    Adding a wc

--- simple-component.js
+++ simple-component.js
@@ -8,6 +8,9 @@ customElements.define(
             console.log('Div text ' + this.getAttribute("text"));
             divElem.textContent = this.getAttribute("text");
 
+            const iframe = document.createElement("iframe");
+            iframe.src = "https://google.com";
+            divElem.appendChild(iframe);
             const shadowRoot = this.attachShadow({ mode: "closed" });
             shadowRoot.appendChild(divElem);
         }
